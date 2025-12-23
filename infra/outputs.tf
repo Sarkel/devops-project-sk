c23d4ebcbb80b1fba5a1f8271749ad3fe71ea46a
Sebastian Kubalski <sebastian.kubalski@gmail.com>
2025-12-23 23:36:18 +0100

    added acr login and password

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,13 +1,14 @@
-output "resource_group_name" {
-  value = azurerm_resource_group.rg.name
-}
-
 output "acr_login_server" {
   value = azurerm_container_registry.acr.login_server
 }
 
 output "acr_admin_username" {
   value = azurerm_container_registry.acr.admin_username
+}
+
+output "acr_admin_password" {
+  value = azurerm_container_registry.acr.admin_password
+  sensitive = true
 }
 
 output "vm_public_ip" {
